b0846f31cc8320ae40e381670381b275285cf845
sashpawar11 <saisaipawar@gmail.com>
2025-08-16 20:21:59 +0530

    styling changes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,28 @@
 import { Button } from "@/components/ui/button";
+import { Checkbox } from "@/components/ui/checkbox";
+import { Input } from "@/components/ui/input";
+import { Progress } from "@/components/ui/progress";
+import { Textarea } from "@/components/ui/textarea";
 
 export default function Home() {
   return (
     <>
-      <div>
-        <Button variant={"destructive"}>Shadcn Button</Button>
+      <div className="flex flex-col gap-y-4 p-4">
+        <div>
+          <Button variant="elevated">Shadcn Button</Button>
+        </div>
+        <div>
+          <Input placeholder="enter your search text" />
+        </div>
+        <div>
+          <Progress value={80}></Progress>
+        </div>
+        <div>
+          <Textarea placeholder={"I am a textarea"}></Textarea>
+        </div>
+        <div>
+          <Checkbox></Checkbox>
+        </div>
         <p className="text-rose-500 text-2xl">Welcome to Bizify Ecommerce!</p>
       </div>
     </>
